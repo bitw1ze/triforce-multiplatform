178001d633bab9e950393c46ebbc801f9c39b9d1
Gabe Pike <gpike75@gmail.com>
2012-04-09 07:27:04 -0700

    fixed 0 case not being displayed

--- proto/pushalg.cpp
+++ proto/pushalg.cpp
@@ -7,7 +7,7 @@
 #define NROW 10
 
 static const char *blockTypes = "!X$+=@";
-int grid[NCOL][NROW] = {0};
+int grid[NCOL][NROW];
 int bottom = NROW-1;
 
 using namespace std;
@@ -15,32 +15,33 @@ using namespace std;
 void generateRow () {
   bottom = (bottom + NROW - 1) % NROW;
   int ntypes = strlen(blockTypes);
-  for (int i=0; i<NCOL; ++i) {
+  for (int i=0; i<NCOL; ++i) 
     grid[bottom][i] = rand() % ntypes;
-  }
 }
 
 void printRow(int row[]) {
   for (int i=0; i<NCOL; ++i) {
-    cout.put (row[i] == 0 ? ' ' : blockTypes [row [i]]);
+    cout.put (row[i] == -1 ? ' ' : blockTypes [row [i]]);
   }
   cout.put('\n');
 }
 
 void printGrid() {
-    for (int i=( (bottom + NROW - 1) % NROW ), j=0; 
-         j < NROW; 
-         i = (i + NROW - 1) % NROW, ++j) {
+    for ( int i=( (bottom + NROW - 1) % NROW ), j=0
+        ; j < NROW
+        ; i = (i + NROW - 1) % NROW, ++j ) 
       printRow (grid[i]);
-    }
 }
 
 int main(void) {
   srand(time(NULL));
 
-  for (int i=NROW/2; i<NROW; ++i) {
+  for (int i=0; i<NROW; ++i)
+    for (int j=0; j<NCOL; ++j)
+      grid[i][j] = -1;
+
+  for (int i=NROW/2; i<NROW; ++i) 
     generateRow ();
-  }
 
   system("clear");
   while (1) {
