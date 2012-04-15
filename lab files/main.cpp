c640e38353e37e80ea29181d957c041133484bb4
Gabe Pike <gpike75@gmail.com>
2012-04-15 00:44:42 -0700

    forgot to add main.cpp

---
+++ lab files/main.cpp
@@ -0,0 +1,34 @@
+#include "game.h"
+
+using namespace std;
+
+void display();
+GameEnv *gameEnv;
+
+void display()
+{
+   gameEnv->ComposeFrame();
+   gameEnv->background.drawGLbackground ();
+
+   for (int i=0; i<NROWS; ++i) 
+	   for (int j=0; j<NCOLS; ++j) 
+		   gameEnv->blocks[i][j].draw(0);
+
+   glutSwapBuffers();
+}
+
+extern void init();
+
+int main(int argc,char** argv)
+{
+	glutInit(&argc, argv);
+
+	init();
+	gameEnv = new GameEnv();
+    glutDisplayFunc(display);
+
+	glutMainLoop();
+	
+
+	return 0;
+}
