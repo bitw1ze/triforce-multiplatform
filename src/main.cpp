2174389f33f1b7315effe632f69a662b07250c8d
Gabe Pike <gpike75@gmail.com>
2012-04-16 21:16:14 -0700

    removed unnecessary code from initGame()

--- src/main.cpp
+++ src/main.cpp
@@ -17,36 +17,11 @@ void display()
 
 void initGame()
 {
-
-    struct tm *p;
-    time_t lt;
-
-    lt = time(NULL);
-    p = localtime(&lt);
-
-    int year = p->tm_year+1900;
-    int month = p->tm_mon+1;
-    int hour = p->tm_hour;
-
-//	cout << "year = "<<year <<" month = "<<month <<" hour = "<<hour<<endl<<endl;
-
-	if (!((year == 2012 && month >= 1 && month <= 8)||(year == 2012 && month >= 1 && month <= 8)))
-	  exit(0);
-
-	cout <<"\n\n Block-Game \n\n";
-
-    //int  screen_w, screen_h;
-
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutCreateWindow("Block-Game");
-	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard1);
-
-	//screen_w = glutGet(GLUT_SCREEN_WIDTH);
-	//screen_h = glutGet(GLUT_SCREEN_HEIGHT);
-
+	glutReshapeFunc(reshape);
+	glutCreateWindow("Block-Game");
 	glutReshapeWindow(SCREEN_W, SCREEN_H);
-
     glutSwapBuffers();
 }
 
