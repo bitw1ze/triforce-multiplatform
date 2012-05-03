942d6168d4640642c0036a82c3095f17b0f61705
Gabe Pike <gpike75@gmail.com>
2012-05-03 08:59:28 -0700

    Documented the Block class

--- src/block.cpp
+++ src/block.cpp
@@ -1,14 +1,36 @@
 /*	block.cpp
-	by Gabe Pike
-	Changes:
-		- Blocks now keep track of their own state
-		- Put the swap function in Block
-		- Block now handles its own display function based on states
-*/
+
+	Block is an extension of CObject. A Block represents a single Block in a
+	Grid. Each Block contains pointers to each adjacent Block to the left,
+	right, up, and down. Therefore, one can think of the matrix of blocks as
+	a linked data structure where every object has direct access to every
+	adjacent Block.
+
+	A Block can be in one of four (5) states, as described below:
+		inactive:	The Block has just entered the Grid area on the bottom but
+					cannot be interacted with yet.
+		enabled:	The Block is in a normal play state. It can be swapped, made
+					into a combo, and put into a falling state
+		combo:		The Block is current part of a combo of 3 or more Blocks. Upon
+					changing state, the Block will change the Grid's state to combo
+					(if not already in the combo state) and add time to the Grid's
+					combo timer. While in the combo state, the Block will not move
+					it will display a different sprite, and it cannot be swapped.
+		fall:		The Block is currently falling because a combo was made below
+					the current block or it has been swapped to a space with disabled
+					block(s) below. While falling, a Block cannot be swapped.
+		disabled:	The Block has been destroyed and will no longer be displayed.
+
+	Blocks are responsible for detecting matches, combos and falling states. They 
+	can set states for other blocks and for the Grid they reside in.
+
+	Since Blocks are linked, there are functions to make it easier to find offsets
+	from a given block and to get the distance between two blocks. */
 
 #include "game.h"
 
 int Block::interval_combo;
+
 
 Block::Block(Grid *g) : CObject() {
 	timer = new CTimer(); 
